2d6cbb9a47371061a5d46bde090c6b7c2288a5fe
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-02-11 01:44:26 -0600

    added const employee and module exports to all lib javascript files

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-// jest test go in tests folder
 //dist folder for rendered output html and css style sheet
-//lib folder for classes
 //src folder for template helper code
 //index.js runs the application
+
+//employee.js first, all others are based around it, their classes will seperate them from the others but need employee done first
 
 //invoked by running node index.js in terminal/command line
 
 // consider adding validation to ensure user input provided is in the proper expected format, refer to previous node project (#9) for syntax/#phrasing
 
 //tests need to be written for each part of your code and ensure it passes all of the tests you write
-//REMEMBER: you should fail the test first... if you pass the test first there is a problem with your code so break it before you buy it... refer to 10-1 and 10-2 for reference on tests...also like Google...
 
 //make sure code ends being refactored for ES6 as well, lesson 5 in the module.... refer to activity 4 from in class
 
-//make sure you include all relevant information asked for in the project section of module 10...you will generate an html page and css stylesheet from this to  render in a browser... end sresult is html and it will have al information tat you added through the prompts and such
+//make sure you include all relevant information asked for in the project section of module 10...you will generate an html page and css stylesheet from this to  render in a browser... end result is html and it will have all information that you added through the prompts and such
 
 //when you submit, you will need a link to the video showing functionality and add it to your README as well
