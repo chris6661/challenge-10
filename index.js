7c735e6309aade5ef81ba434f72853c20452f947
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-02-14 15:18:57 -0600

    index.js file wrote, added validation to each step asking for info for html file, and formatted

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 //dist folder for rendered output html and css style sheet
-//index.js runs the application
-
-//invoked by running node index.js in terminal/command line
+//index.js runs the application with node
 
 // consider adding validation to ensure user input provided is in the proper expected format, refer to previous node project (#9) for syntax/#phrasing
 
@@ -13,7 +11,209 @@
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern'); 
+const Intern = require('./lib/Intern');
 const inquirer = require("inquirer"); //install
-const fs = require("fs"); 
-const render = require('./lib/Renderhtml'); 
+const fs = require('fs');
+const render = require('./lib/Renderhtml');
+
+const people = [];
+
+const createManager = (personInfo) => {
+    const manager = new Manager(personInfo.managername, personInfo.managerid, personInfo.manageremail, personInfo.manageroffice);
+    people.push(manager);
+    loop();
+};
+
+const createEngineer = (personInfo) => {
+    const engineer = new Engineer(personInfo.engineername, personInfo.engineerid, personInfo.engineeremail, personInfo.engineergit);
+    people.push(engineer);
+    loop();
+};
+
+const createIntern = (personInfo) => {
+    const intern = new Intern(personInfo.internname, personInfo.internid, personInfo.internemail, personInfo.internschool)
+    people.push(intern);
+    loop();
+};
+const getPersonInfo = () => {
+    inquirer.prompt([{
+            type: "input",
+            message: "What is the manager's name?",
+            name: "managername",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid manager name is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the manager's ID?",
+            name: "managerid",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid manager id is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the manager's email?",
+            name: "manageremail",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid manager email is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "manageroffice",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid maanger office number is required.");
+                }
+                return true;
+            }
+        },
+
+    ]).then((managerInfo) => [
+        createManager(managerInfo)
+    ])
+};
+
+const getEngineerInfo = () => {
+    inquirer.prompt([{
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "engineername",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid engineer name is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the engineer's id?",
+            name: "engineerid",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid engineer id is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the engineer's email?",
+            name: "engineeremail",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid engineer email is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the engineer's GitHub?",
+            name: "engineergit",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid engineer GitHub is required.");
+                }
+                return true;
+            }
+        }
+
+    ]).then((engineerInfo) => {
+        createEngineer(engineerInfo)
+    })
+
+}
+
+const getInternInfo = () => {
+    inquirer.prompt([{
+            type: "input",
+            message: "What is the intern's name?",
+            name: "internname",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid intern name is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the intern's id?",
+            name: "internid",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid intern id is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the intern's email?",
+            name: "internemail",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid intern email is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the intern's school?",
+            name: "internschool",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid school is required.");
+                }
+                return true;
+            }
+        }
+
+    ]).then((internInfo) => {
+        createIntern(internInfo)
+    })
+
+}
+
+const loop = () => {
+    inquirer.prompt([{
+            type: "list",
+            name: "teammember",
+            choices: [
+                "Engineer", "Intern", "I do not want to add any more team members."
+            ]
+        }])
+        .then((personInfo) => {
+
+            switch (personInfo.teammember) {
+                case 'I do not want to add any more team members.':
+                    createTeam();
+                    break;
+
+                case 'Engineer':
+                    getEngineerInfo()
+                    break;
+
+                case 'Intern':
+                    getInternInfo()
+                    break;
+            }
+        })
+};
+
+getPersonInfo();
