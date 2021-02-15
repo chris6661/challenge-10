35d4c8529e49d2487f7efeb8cacec0f240f4fb41
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-02-15 13:23:19 -0600

    rendering html page with requested information

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 //dist folder for rendered output html and css style sheet
 //index.js runs the application with node
 
-// consider adding validation to ensure user input provided is in the proper expected format, refer to previous node project (#9) for syntax/#phrasing
-
-//make sure you include all relevant information asked for in the project section of module 10...you will generate an html page and css stylesheet from this to  render in a browser... end result is html and it will have all information that you added through the prompts and such
-
 //when you submit, you will need a link to the video showing functionality and add it to your README as well
+
 //variables to make file/functions work
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
